c4212d646ccc5ff48c79138cdc1b3d8cd93eda53
Naman <naman@iiitb.online>
2022-04-10 22:44:25 +0530

    Changes.

---
+++ src/main/java/com/example/pe/dao/UserDao.java
@@ -0,0 +1,7 @@
+package com.example.pe.dao;
+
+import com.example.pe.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserDao extends JpaRepository<User,Long> {
+}
