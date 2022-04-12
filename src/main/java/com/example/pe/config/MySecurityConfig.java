3c02bdce9102c25f1e760e84d46a951112136481
Naman <naman@iiitb.online>
2022-04-12 13:26:51 +0530

    Changes.

---
+++ src/main/java/com/example/pe/config/MySecurityConfig.java
@@ -0,0 +1,57 @@
+package com.example.pe.config;
+
+import com.example.pe.service.userServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http
+                //csrf disable for api testing , please turn on when we use on browser.
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/user","/adduser").permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+   // @Override
+    //protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+      //  auth.userDetailsService(User);
+    //}
+
+    //@Bean
+    //public BCryptPasswordEncoder  passwordEncoder(){
+      //  return new BCryptPasswordEncoder(20);
+    //}
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+
+
+
+
+
+}
